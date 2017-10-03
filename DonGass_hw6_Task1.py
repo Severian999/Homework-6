1ce70609fb61ad73a2130b59a3e8f65aa7147664
Severian999 <don.gass@gmail.com>
2017-10-02 18:51:51 -0600

    Add files via upload

---
+++ DonGass_hw6_Task1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  2 18:23:34 2017
+
+@author: Don Gass
+"""
+
+def explode_seconds(seconds):
+    years, secsr = divmod(seconds, 31536000)
+    days, secsr = divmod(secsr, 86400)
+    hours, secsr = divmod(secsr, 3600)
+    minutes, secsr = divmod(secsr, 60)
+    return (years, days, hours, minutes, secsr)
+
+print(explode_seconds(1000000000000))
+exploded = explode_seconds(1000000000000)
+print('1 trillion seconds is %d years, %d days, %d hours, %d minutes, and %d seconds' \
+      % (exploded[0], exploded[1], exploded[2], exploded[3], exploded[4]))
